6a202a5fea30b5ff7199e8aa37dfc483aa378d13
Daniel ceron <desceron@unicauca.edu.co>
2022-12-19 21:25:38 -0500

    linkedin added

--- src/components/ContactButtons/ContactButtons.tsx
+++ src/components/ContactButtons/ContactButtons.tsx
@@ -4,22 +4,22 @@ import classes from './ContactButtons.module.css';
 
 import { ReactComponent as GithubSvg } from 'assets/svg/github.svg';
 import { ReactComponent as CvSvg } from 'assets/svg/cv.svg';
+import { ReactComponent as LinkedinSvg } from 'assets/svg/linkedin.svg';
 import Cv from 'assets/pdf/cv.pdf';
 
 
-const ContactButtons:React.FC = () => {
-    
-    return <div className ={classes.base}>
-        <button>
-            <a href="https://github.com/DanielCeron-dc" style={{ display: 'flex', height: '100%', alignItems: 'center',  justifyContent: 'center', textDecoration: 'none', color: 'white'}}>
-                Github{"  "}<GithubSvg style={{ width: 50, height: 50, fill: 'white', margin: '0 10px'}} />
-            </a>
-        </button>
-        <button>
-            <a href={Cv} download style={{ display: 'flex', height: '100%', alignItems: 'center', justifyContent: 'center', textDecoration: 'none', color: 'white' }}>
-                CV{' '}<CvSvg style={{ width: 50, height: 50, fill: 'white', margin: '0 10px' }} />
-            </a>
-        </button>
+const ContactButtons: React.FC = () => {
+
+    return <div className={classes.base}>
+        <a href="https://github.com/DanielCeron-dc" style={{ display: 'flex', height: '100%', alignItems: 'center', justifyContent: 'center', textDecoration: 'none', color: 'white' }}>
+            Github{"  "}<GithubSvg style={{ width: 40, height: 40, fill: 'white', margin: '0 10px' }} />
+        </a>
+        <a href={Cv} download style={{ display: 'flex', height: '100%', alignItems: 'center', justifyContent: 'center', textDecoration: 'none', color: 'white' }}>
+            CV{' '}<CvSvg style={{ width: 40, height: 40, fill: 'white', margin: '0 10px' }} />
+        </a>
+        <a href="https://www.linkedin.com/in/daniel-ceron-b10612200/" download style={{ display: 'flex', height: '100%', alignItems: 'center', justifyContent: 'center', textDecoration: 'none', color: 'white' }}>
+            LinkedIn{' '}<LinkedinSvg style={{ width: 40, height: 40, fill: 'white', margin: '0 10px' }} />
+        </a>
     </div>
 }
 export default ContactButtons;
