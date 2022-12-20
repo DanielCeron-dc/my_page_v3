f8c0aba99f3874e217a0e570c95264dda3f24047
Daniel ceron <desceron@unicauca.edu.co>
2022-12-19 21:31:44 -0500

    noew links open in a new tab

--- src/components/ContactButtons/ContactButtons.tsx
+++ src/components/ContactButtons/ContactButtons.tsx
@@ -11,13 +11,26 @@ import Cv from 'assets/pdf/cv.pdf';
 const ContactButtons: React.FC = () => {
 
     return <div className={classes.base}>
-        <a href="https://github.com/DanielCeron-dc" style={{ display: 'flex', height: '100%', alignItems: 'center', justifyContent: 'center', textDecoration: 'none', color: 'white' }}>
-            Github{"  "}<GithubSvg style={{ width: 40, height: 40, fill: 'white', margin: '0 10px' }} />
+        <a
+            href="https://github.com/DanielCeron-dc"
+            style={{ display: 'flex', height: '100%', alignItems: 'center', justifyContent: 'center', textDecoration: 'none', color: 'white' }}
+            target="_blank"
+        >
+            Github{"  "} <GithubSvg style={{ width: 40, height: 40, fill: 'white', margin: '0 10px' }} />
         </a>
-        <a href={Cv} download style={{ display: 'flex', height: '100%', alignItems: 'center', justifyContent: 'center', textDecoration: 'none', color: 'white' }}>
-            CV{' '}<CvSvg style={{ width: 40, height: 40, fill: 'white', margin: '0 10px' }} />
+        <a
+            href={Cv}
+            download
+            style={{ display: 'flex', height: '100%', alignItems: 'center', justifyContent: 'center', textDecoration: 'none', color: 'white' }}
+        >
+            CV{' '} <CvSvg style={{ width: 40, height: 40, fill: 'white', margin: '0 10px' }}/>
         </a>
-        <a href="https://www.linkedin.com/in/daniel-ceron-b10612200/" download style={{ display: 'flex', height: '100%', alignItems: 'center', justifyContent: 'center', textDecoration: 'none', color: 'white' }}>
+        <a
+            href="https://www.linkedin.com/in/daniel-ceron-b10612200/"
+            download
+            style={{ display: 'flex', height: '100%', alignItems: 'center', justifyContent: 'center', textDecoration: 'none', color: 'white' }}
+            target="_blank"
+        >
             LinkedIn{' '}<LinkedinSvg style={{ width: 40, height: 40, fill: 'white', margin: '0 10px' }} />
         </a>
     </div>
