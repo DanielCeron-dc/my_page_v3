8176829a8e78836596390a93bbdbdb7b1fea63cf
Daniel ceron <desceron@unicauca.edu.co>
2022-12-19 20:52:14 -0500

    config improved

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,5 +1,8 @@
+
+console.log(process.env.NODE_ENV);
+
 export const config = {
-    apiUrl: process.env.REACT_APP_API_URL || 'http://localhost:5000',
+    apiUrl: process.env.NODE_ENV === 'development' ? process.env.REACT_APP_API_URL_DEV : process.env.REACT_APP_API_URL,
     aws: {
         accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID || '',
         secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY || '',
